016895eb649f190d4e9c94bfc6fd228a9bc92a61
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-24 23:50:35 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://eco-platform.onrender.com/api";
 
 async function login() {
   const email = document.getElementById("email").value;
