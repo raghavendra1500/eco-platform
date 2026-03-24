d34efb712d75c457af6c3e75c9fed0ce5d002bea
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-24 14:31:46 +0530

    Add User model with schema definition

--- models/User.js
+++ models/User.js
@@ -1,1 +1,15 @@
+const mongoose = require("mongoose");
 
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  school: String,
+  ecoPoints: {
+    type: Number,
+    default: 0
+  },
+  badges: [String]
+});
+
+module.exports = mongoose.model("User", userSchema);
