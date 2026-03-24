2625543e60aaa437cc8e67f1beb81265f8d51f8f
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-24 14:32:55 +0530

    Implement task retrieval and creation endpoints

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,1 +1,25 @@
+const Task = require("../models/Task");
 
+// GET ALL TASKS
+exports.getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// CREATE TASK
+exports.createTask = async (req, res) => {
+  try {
+    const { title, description, points } = req.body;
+
+    const task = new Task({ title, description, points });
+    await task.save();
+
+    res.status(201).json(task);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
