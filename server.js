cea338d3cf2c4f90758d46aacf7f46e508d1c661
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-24 14:57:27 +0530

    Add task and submission routes to server

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ app.use(express.json());
 
 // Routes
 const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+const submissionRoutes = require("./routes/submissionRoutes");
 
 app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/submissions", submissionRoutes);
 
 app.get("/", (req, res) => {
   res.send("API Running");
 });
 
 app.listen(5000, () => {
-  console.log("Server running on port 5000");
+  console.log("Server running");
 });
