6e51de8b92ba37fbd0af71d6b5fff52030efd00e
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-24 14:56:30 +0530

    Add new routes for getting and creating tasks

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const taskController = require("../controllers/taskController");
 
+// GET ALL TASKS
 router.get("/", taskController.getTasks);
+
+// CREATE TASK
 router.post("/", taskController.createTask);
 
 module.exports = router;
