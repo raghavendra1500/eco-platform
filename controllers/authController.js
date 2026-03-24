253dc1896e7d3f0c775814570e214bfcb14aa38d
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-24 14:32:38 +0530

    Add user registration functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,21 @@
+const User = require("../models/User");
 
+// REGISTER
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password, school } = req.body;
+
+    const user = new User({
+      name,
+      email,
+      password,
+      school
+    });
+
+    await user.save();
+
+    res.status(201).json({ message: "User registered", user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
