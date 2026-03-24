90880a99860d179641e7d3d92b85818cdd874344
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-24 14:52:42 +0530

    Implement JWT authentication middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,1 +1,19 @@
+const jwt = require("jsonwebtoken");
 
+module.exports = (req, res, next) => {
+  try {
+    const token = req.header("Authorization");
+
+    if (!token) {
+      return res.status(401).json({ message: "No token, access denied" });
+    }
+
+    const decoded = jwt.verify(token, "SECRET_KEY");
+
+    req.user = decoded;
+    next();
+
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
