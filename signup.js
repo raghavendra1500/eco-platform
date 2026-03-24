9a84b01e90212948589974ed679098c29fb08867
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-24 15:14:04 +0530

    Create signup.js

---
+++ signup.js
@@ -0,0 +1,29 @@
+const BASE_URL = "http://localhost:5000/api";
+
+async function signup() {
+  const name = document.getElementById("name").value;
+  const school = document.getElementById("school").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const res = await fetch(`${BASE_URL}/auth/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      name,
+      school,
+      email,
+      password
+    })
+  });
+
+  const data = await res.json();
+
+  alert(data.message);
+
+  if (res.status === 201) {
+    window.location.href = "login.html";
+  }
+}
