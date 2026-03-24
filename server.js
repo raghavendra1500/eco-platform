8fff161f1530a1836b5bba0c02bc81236521c4ae
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-24 14:37:45 +0530

    Set up Express server with task and submission routes

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+const express = require("express");
+const app = express();
 
+app.use(express.json());
+
+const taskRoutes = require("./routes/taskRoutes");
+const submissionRoutes = require("./routes/submissionRoutes");
+
+app.use("/api/tasks", taskRoutes);
+app.use("/api/submissions", submissionRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+app.listen(5000, () => {
+  console.log("Server started");
+});
