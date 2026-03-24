73ed4269642a577d71ad69b8088337abc370cbdb
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-24 14:32:14 +0530

    Create Submission model with schema definition

--- models/Submission.js
+++ models/Submission.js
@@ -1,1 +1,19 @@
+const mongoose = require("mongoose");
 
+const submissionSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  taskId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Task"
+  },
+  image: String,
+  status: {
+    type: String,
+    default: "pending"
+  }
+});
+
+module.exports = mongoose.model("Submission", submissionSchema);
