027f1145daa37994699cbbf5fcba445a91c7f3f7
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-25 14:36:11 +0530

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,1 +1,47 @@
+import { useEffect, useState } from "react";
+import API from "../services/api";
 
+export default function Dashboard() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    const res = await API.get("/tasks");
+    setTasks(res.data);
+  };
+
+  const submitTask = async (taskId) => {
+    const token = localStorage.getItem("token");
+
+    await API.post(
+      "/submissions",
+      { taskId, proof: "image-url" },
+      {
+        headers: { Authorization: token }
+      }
+    );
+
+    alert("Submitted!");
+  };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Dashboard 🌱</h1>
+
+      {tasks.map((task) => (
+        <div key={task._id} style={{ border: "1px solid", margin: "10px", padding: "10px" }}>
+          <h3>{task.title}</h3>
+          <p>{task.description}</p>
+          <p>{task.points} points</p>
+
+          <button onClick={() => submitTask(task._id)}>
+            Submit
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
