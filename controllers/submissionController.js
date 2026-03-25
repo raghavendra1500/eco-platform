fc111daf8325b061a7c5565940fd8786ec7aecf0
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-25 10:37:02 +0530

    Update submissionController.js

--- controllers/submissionController.js
+++ controllers/submissionController.js
@@ -2,27 +2,26 @@ const Submission = require("../models/Submission");
 const Task = require("../models/Task");
 const User = require("../models/User");
 
-// SUBMIT TASK
-exports.submitTask = async (req, res) => {
-  try {
-    const { taskId, image } = req.body;
+// task submission
+exports.createSubmission = async (req, res) => {
+  const { taskId, image } = req.body;
 
-    const submission = new Submission({
-      userId: req.user.id, // from auth middleware
-      taskId,
-      image
-    });
+  const task = await Task.findById(taskId);
+  const user = await User.findById(req.user.id);
 
-    await submission.save();
+  const submission = new Submission({
+    user: req.user.id,
+    task: taskId,
+    image
+  });
 
-    res.status(201).json({
-      message: "Task submitted for review",
-      submission
-    });
+  await submission.save();
 
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+  // ADD POINTS 🔥
+  user.ecoPoints += task.points;
+  await user.save();
+
+  res.status(201).json({ message: "Task submitted" });
 };
 
 // APPROVE TASK (Teacher/Admin)
