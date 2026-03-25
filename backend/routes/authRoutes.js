bab91013701668745f0e571422fb1a07fc70103b
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-25 14:26:00 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const router = express.Router();
 
+const {
+  register,
+  login,
+  getLeaderboard
+} = require("../controllers/authController");
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/leaderboard", getLeaderboard);
+
+module.exports = router;
