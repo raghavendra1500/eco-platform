17d0f632774a22aceecec6f9b682ea1c7842a5fb
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-25 14:24:18 +0530

    Update Submission.js

--- backend/models/Submission.js
+++ backend/models/Submission.js
@@ -1,1 +1,19 @@
+const mongoose = require("mongoose");
 
+const submissionSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  task: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Task"
+  },
+  proof: String,
+  status: {
+    type: String,
+    default: "pending"
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Submission", submissionSchema);
