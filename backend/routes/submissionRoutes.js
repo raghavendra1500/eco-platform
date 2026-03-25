4e38112c7854dab031d57be28e9afd8930c9ca6c
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-25 14:26:26 +0530

    Update submissionRoutes.js

--- backend/routes/submissionRoutes.js
+++ backend/routes/submissionRoutes.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const router = express.Router();
 
+const { submitTask } = require("../controllers/submissionController");
+const { protect } = require("../middleware/authMiddleware");
+
+router.post("/", protect, submitTask);
+
+module.exports = router;
