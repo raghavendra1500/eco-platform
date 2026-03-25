bf92de402ca7dce381bf3caeb64b21bcb8fa996d
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-25 14:25:37 +0530

    Update submissionController.js

--- backend/controllers/submissionController.js
+++ backend/controllers/submissionController.js
@@ -1,1 +1,13 @@
+const Submission = require("../models/Submission");
 
+exports.submitTask = async (req, res) => {
+  const { taskId, proof } = req.body;
+
+  const submission = await Submission.create({
+    user: req.user.id,
+    task: taskId,
+    proof
+  });
+
+  res.json(submission);
+};
