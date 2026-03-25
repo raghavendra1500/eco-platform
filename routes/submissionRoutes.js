af25885ab3053927ce2f2b7ce5c221786bf242ae
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-25 12:06:02 +0530

    Delete routes directory

--- routes/submissionRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const submissionController = require("../controllers/submissionController");
-const { protect } = require("../middleware/authMiddleware");
-
-router.post("/", protect, submissionController.submitTask);
-
-module.exports = router;
