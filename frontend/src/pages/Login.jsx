3ef8555a77dbe86e5bd4b1fa043205049737aaa5
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-25 14:35:27 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,1 +1,33 @@
+import { useState } from "react";
+import API from "../services/api";
 
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    try {
+      const res = await API.post("/auth/login", { email, password });
+
+      localStorage.setItem("token", res.data.token);
+      alert("Login success");
+
+      window.location.href = "/dashboard";
+    } catch (err) {
+      alert("Login failed");
+    }
+  };
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h2>Login 🌱</h2>
+
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} /><br/><br/>
+      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} /><br/><br/>
+
+      <button onClick={login}>Login</button>
+
+      <p onClick={() => window.location.href="/signup"}>Signup</p>
+    </div>
+  );
+}
