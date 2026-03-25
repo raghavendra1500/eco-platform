43c0cbecc262bddf2555e8a93ad0c584f5bf2991
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-25 10:32:38 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -36,37 +36,30 @@ exports.register = async (req, res) => {
 
 // LOGIN
 exports.login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    // Check user
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({ message: "Invalid credentials" });
-    }
+  const user = await User.findOne({ email });
 
-    // Compare password
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ message: "Invalid credentials" });
-    }
-
-    // Create token
-    const token = jwt.sign(
-      { id: user._id },
-      "SECRET_KEY",   // later move to env
-      { expiresIn: "7d" }
-    );
-
-    res.json({
-      message: "Login successful",
-      token,
-      user
-    });
-
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+  if (!user) {
+    return res.status(400).json({ message: "User not found" });
   }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+  res.json({
+    token,
+    user: {
+      _id: user._id,
+      name: user.name,
+      ecoPoints: user.ecoPoints
+    }
+  });
 };
 
 // GET LEADERBOARD
