c9928ddcd1ece5157352bd328efcda051a34a847
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-25 10:34:43 +0530

    Update dashboard.js

--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -69,9 +69,20 @@ async function loadLeaderboard() {
   });
 }
 //Load User Info
-function loadUser() {
-  // later you fetch from backend
-  document.querySelector(".topbar h3").innerText = "Welcome, Student 👋";
+async function loadUser() {
+  const res = await fetch(`${BASE_URL}/auth/me`, {
+    headers: {
+      Authorization: localStorage.getItem("token")
+    }
+  });
+
+  const user = await res.json();
+
+  document.querySelector(".topbar h3").innerText =
+    `Welcome, ${user.name} 👋`;
+
+  document.querySelector(".topbar span").innerHTML =
+    `Eco Points: <strong>${user.ecoPoints}</strong>`;
 }
 
 // dashboard for real data
