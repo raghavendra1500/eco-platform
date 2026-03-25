c04dcb91c3136d68903ff56aa828dcf928744931
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-25 14:23:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect DB
+connectDB();
+
+// Routes
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/tasks", require("./routes/taskRoutes"));
+app.use("/api/submissions", require("./routes/submissionRoutes"));
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running");
+});
