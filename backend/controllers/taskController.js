df6dec3d19de3ccf55b5cfbfefa871735e0053bc
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-25 14:25:21 +0530

    Update taskController.js

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,1 +1,14 @@
+const Task = require("../models/Task");
 
+exports.getTasks = async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+};
+
+exports.createTask = async (req, res) => {
+  const { title, description, points } = req.body;
+
+  const task = await Task.create({ title, description, points });
+
+  res.json(task);
+};
