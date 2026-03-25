370fe3bfa1804a53d81bc7fe85bdacefc76c34e2
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-25 10:26:20 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -98,3 +98,13 @@ exports.getSchoolLeaderboard = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+// dashboard for real data
+exports.getMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
