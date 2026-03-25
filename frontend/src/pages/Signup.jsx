2cc80437d1ecf7346b2a8e037eb1c53193d82c32
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-25 14:35:54 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,1 +1,38 @@
+import { useState } from "react";
+import API from "../services/api";
 
+export default function Signup() {
+  const [form, setForm] = useState({
+    name: "",
+    school: "",
+    email: "",
+    password: ""
+  });
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const signup = async () => {
+    try {
+      await API.post("/auth/register", form);
+      alert("Signup success");
+      window.location.href = "/";
+    } catch {
+      alert("Error");
+    }
+  };
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h2>Signup 🌱</h2>
+
+      <input name="name" placeholder="Name" onChange={handleChange} /><br/><br/>
+      <input name="school" placeholder="School" onChange={handleChange} /><br/><br/>
+      <input name="email" placeholder="Email" onChange={handleChange} /><br/><br/>
+      <input name="password" type="password" placeholder="Password" onChange={handleChange} /><br/><br/>
+
+      <button onClick={signup}>Signup</button>
+    </div>
+  );
+}
