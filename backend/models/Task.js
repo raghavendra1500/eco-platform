ae470c15570cb8dc9d48750a0f6d7431aecca4c0
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-25 14:24:04 +0530

    Update Task.js

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,1 +1,9 @@
+const mongoose = require("mongoose");
 
+const taskSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  points: Number
+}, { timestamps: true });
+
+module.exports = mongoose.model("Task", taskSchema);
