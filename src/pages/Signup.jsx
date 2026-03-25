6006cb6a3e6dc4a4efc2b7e8cb84260d9ffa4c00
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-25 11:54:21 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
 import API from "../services/api";
 
+import { motion } from "framer-motion";
+
+<motion.div
+  initial={{ opacity: 0, y: 40 }}
+  animate={{ opacity: 1, y: 0 }}
+  transition={{ duration: 0.5 }}
+>
+
 export default function Signup() {
   const [form, setForm] = useState({
     name: "",
