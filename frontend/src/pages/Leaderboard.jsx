937f26fdb8993f813d61d324ea7a3b2d6a02886e
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-25 14:36:26 +0530

    Update Leaderboard.jsx

--- frontend/src/pages/Leaderboard.jsx
+++ frontend/src/pages/Leaderboard.jsx
@@ -1,1 +1,27 @@
+import { useEffect, useState } from "react";
+import API from "../services/api";
 
+export default function Leaderboard() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchLeaderboard();
+  }, []);
+
+  const fetchLeaderboard = async () => {
+    const res = await API.get("/auth/leaderboard");
+    setUsers(res.data);
+  };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Leaderboard 🏆</h1>
+
+      {users.map((u, index) => (
+        <div key={index}>
+          {index + 1}. {u.name} - {u.ecoPoints}
+        </div>
+      ))}
+    </div>
+  );
+}
