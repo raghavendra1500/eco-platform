9bc8b1c954e646a991b96200391e813629e6bc28
raghavendra1500 <23211a6742@bvrit.ac.in>
2026-03-26 14:22:08 +0530

    Update submissionController.js

--- backend/controllers/submissionController.js
+++ backend/controllers/submissionController.js
@@ -51,3 +51,32 @@ exports.rejectSubmission = async (req, res) => {
 
   res.json({ message: "Rejected" });
 };
+
+
+
+const cloudinary = require("../config/cloudinary");
+const Submission = require("../models/Submission");
+
+// USER SUBMIT WITH IMAGE
+exports.submitTask = async (req, res) => {
+  try {
+    const { taskId, image } = req.body;
+
+    // Upload to Cloudinary
+    const upload = await cloudinary.uploader.upload(image, {
+      folder: "eco-platform"
+    });
+
+    const submission = await Submission.create({
+      user: req.user.id,
+      task: taskId,
+      proof: upload.secure_url
+    });
+
+    res.json(submission);
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Upload failed" });
+  }
+};
